246ca3318c8b69ff8a4c9362d2001c4085498e4e
Hiteshree277 <73422564+Hiteshree277@users.noreply.github.com>
2020-11-21 20:11:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,45 +1,56 @@
-var car;
-var wall;
+var bullet;
+var wall,thickness;
 var speed;
-var waight;
+var weight;
 
 
 
 function setup() {
   createCanvas(1600,400);
   
-  car=createSprite(50,200,60,60);
-  wall=createSprite(1500,200,60,height/2);
+  bullet=createSprite(50,200,17,5);
+  bullet.shapeColor=color(0);
+  wall=createSprite(1500,200,thickness,height/2);
 
   
   
-  speed=random(55,90);
-  weight=random(400,1500);
+  speed=random(223,321);
+  weight=random(30,52);
+  thickness=random(22,83);
 
 }
 
 function draw() {
-  background(55);  
-  car.velocityX=speed;
-  car.collide(wall);
+  background(255);  
+  bullet.velocityX=speed;
+  //bullet.collide(wall);
 
-if(wall.x-car.x <=(car.width+wall.width)/2){
+if(collide(bullet,wall)){
+bullet.velocityX=0;
+var damage=0.5*weight*speed*speed/(thickness*thickness*thickness);
 
-car.velocityX=0;
-var deformation=0.5 * weight * speed * speed/22500;
-if(deformation>180){
-car.shapeColor=color(255.0,0);
+if(damage>10){
+  wall.shapeColor=color(255,0,0);
 }
 
-if(deformation<180 && deformation>100){
-  car.shapeColor=color(230,230,0);
+if(damage<10){
+  wall.shapeColor=color(0,255,0);
 }
 
-if(deformation<100){
-  car.shapeColor=color(0,255,0);
-}
 
 }
 
   drawSprites();
+}
+
+function collide(bullet,wall){
+
+bulletRightEdge=bullet.x + bullet.width;
+wallLeftEdge=wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+  return true 
+}
+return false;
+
+
 }
